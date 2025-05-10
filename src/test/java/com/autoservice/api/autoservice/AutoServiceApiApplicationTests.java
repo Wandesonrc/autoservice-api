77e91e1b6a7c4913afe431bdf385ed9ee5844511
Wandeson Rodrigues <wandesonrc@gmail.com>
2025-05-10 08:27:07 -0300

    Initial commit: Projeto Spring Boot AutoService API

---
+++ src/test/java/com/autoservice/api/autoservice/AutoServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoservice.api.autoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
