de9061a67e3edfdd6cf0ffdeaa45cf408d054867
Wandeson Rodrigues <wandesonrc@gmail.com>
2025-05-15 21:35:50 -0300

    feat: cria entidade Produto

---
+++ src/main/java/com/autoservice/api/autoservice/model/Produto.java
@@ -0,0 +1,86 @@
+package com.autoservice.api.autoservice.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+@Entity
+@Table(name = "TB_PRODUTO")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String descricao;
+
+    private Integer quantidade;
+
+    @Column(name = "preco_venda")
+    private BigDecimal precoVenda;
+
+    @Column(name = "preco_compra")
+    private BigDecimal precoCompra;
+
+    private String categoria;
+
+    public Produto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoVenda() {
+        return precoVenda;
+    }
+
+    public void setPrecoVenda(BigDecimal precoVenda) {
+        this.precoVenda = precoVenda;
+    }
+
+    public BigDecimal getPrecoCompra() {
+        return precoCompra;
+    }
+
+    public void setPrecoCompra(BigDecimal precoCompra) {
+        this.precoCompra = precoCompra;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
