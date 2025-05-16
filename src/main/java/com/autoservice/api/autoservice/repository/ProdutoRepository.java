68f4f64e611c249c4c3dca7a8f91171947f11dbb
Wandeson Rodrigues <wandesonrc@gmail.com>
2025-05-15 21:53:38 -0300

    feat: adionar ProdutoRepository

---
+++ src/main/java/com/autoservice/api/autoservice/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.autoservice.api.autoservice.repository;
+
+import com.autoservice.api.autoservice.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto,Long> {
+}
