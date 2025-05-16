b57c292042edc06efec0fd2140380e96a2e2e326
Wandeson Rodrigues <wandesonrc@gmail.com>
2025-05-15 22:33:19 -0300

    feat: implementar ProdutoService

---
+++ src/main/java/com/autoservice/api/autoservice/service/ProdutoService.java
@@ -0,0 +1,27 @@
+package com.autoservice.api.autoservice.service;
+
+import com.autoservice.api.autoservice.model.Produto;
+import com.autoservice.api.autoservice.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto salvarProduto(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Optional<Produto> buscarPorId(Long id){
+        return produtoRepository.findBy(id);
+    }
+
+    public List<Produto> listarProdutos(){
+        return produtoRepository.findAll();
+    }
+}
