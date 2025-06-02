5879903367975bd8fd4ae192673ec0c4f5552946
Wandeson Rodrigues <wandesonrc@gmail.com>
2025-06-02 18:59:08 -0300

    feat: adiciona endpoint de criação de Serviço

---
+++ src/main/java/com/autoservice/api/autoservice/controller/ServicoController.java
@@ -0,0 +1,25 @@
+package com.autoservice.api.autoservice.controller;
+
+import com.autoservice.api.autoservice.dto.ServicoDTO;
+import com.autoservice.api.autoservice.model.Servico;
+import com.autoservice.api.autoservice.service.ServicoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class ServicoController {
+
+    @Autowired
+    private ServicoService servicoService;
+
+    public ResponseEntity<Servico> criarServico(@Valid ServicoDTO dto){
+        Servico servico = servicoService.criarServico(dto);
+        return new ResponseEntity<>(servico, HttpStatus.CREATED);
+    }
+
+}
