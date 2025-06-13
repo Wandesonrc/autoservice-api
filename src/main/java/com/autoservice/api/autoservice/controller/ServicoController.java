73ecbe340a8b63cc437c29966bf60b8b9e69642d
Wandeson Rodrigues <wandesonrc@gmail.com>
2025-06-13 10:38:36 -0300

    feat: adiciona o PostMapping

--- src/main/java/com/autoservice/api/autoservice/controller/ServicoController.java
+++ src/main/java/com/autoservice/api/autoservice/controller/ServicoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,9 +18,11 @@ public class ServicoController {
     @Autowired
     private ServicoService servicoService;
 
+    @PostMapping("/servico")
     public ResponseEntity<Servico> criarServico(@Valid ServicoDTO dto){
         Servico servico = servicoService.criarServico(dto);
         return new ResponseEntity<>(servico, HttpStatus.CREATED);
     }
+
 
 }
