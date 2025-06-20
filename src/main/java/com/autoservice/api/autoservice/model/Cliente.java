02b4dd4c9c2b0fa98e3a757fbf49d62ce69c8f23
Wandeson Rodrigues <wandesonrc@gmail.com>
2025-06-19 22:34:13 -0300

    feat: cria entidade Cliente

---
+++ src/main/java/com/autoservice/api/autoservice/model/Cliente.java
@@ -0,0 +1,65 @@
+package com.autoservice.api.autoservice.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+@Entity
+@Table(name = "TB_CLIENTE")
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private String email;
+
+    private String telefone;
+
+    @Column(name = "data_cadastro")
+    private LocalDate dataCadastro;
+
+    public Cliente() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+}
